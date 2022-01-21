ede1c7e0deb900b45b4f4cca6bf706ae29f35c14
Kelliep11 <kellie.price@lv.co.uk>
2022-01-21 15:39:22 +0000

    Add item to todo list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+from flask import request
 from todo_app.data.session_items import get_items
+from todo_app.data.session_items import add_item
 
 from todo_app.flask_config import Config 
 
@@ -8,7 +10,18 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     items = get_items()
     return render_template('index.html', items = items)
+
+@app.route('/todo', methods=['GET', 'POST']) 
+def addItem(): 
+        items = get_items()
+        
+        new_item = request.form.get('Todo')
+        add_item(new_item)
+        
+       
+        return render_template('index.html', items = items)
+       
