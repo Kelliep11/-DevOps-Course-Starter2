af7f86ac63e2f7d78175565ddcc6b6eca10609cb
Kelliep11 <kellie.price@lv.co.uk>
2022-02-17 11:07:18 +0000

    instructions added to README

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 from todo_app.data.session_items import get_items, add_item
 
 from todo_app.flask_config import Config 
+import requests
+import dotenv
+import os
 
 
 app = Flask(__name__)
@@ -10,8 +13,25 @@ app.config.from_object(Config())
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    items = get_items()
+
+    url = "https://api.trello.com/1/boards/620a2eb50101820be8503973/"
+
+    print(os.getenv("TRELLO_API_KEY"))
+
+    querystring = {
+    "key":os.getenv("TRELLO_API_KEY"),
+    "token":os.getenv("TRELLO_API_TOKEN"),
+    "cards": "open"
+    }
+    headers = {"content-type": "application/json"}
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response_json = response.json()
+
+    items = response_json['cards']
+
     return render_template('index.html', items = items)
+
 
 @app.route('/todo', methods=['GET', 'POST']) 
 def addItem(): 
