1161d4410f81f53c593b95603fac7da21f55f612
Kelliep11 <kellie.price@lv.co.uk>
2022-11-04 11:45:08 +0000

    work on module 11

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 from todo_app.data.view_model import ViewModel
 
@@ -6,7 +6,7 @@ from todo_app.flask_config import Config
 import requests
 import os
 import todo_app.data.mongo_items as mongo_items
-from flask_login import login_manager, login_required
+from flask_login import LoginManager, login_required
 
 def create_app():
     app = Flask(__name__)
@@ -16,13 +16,14 @@ def create_app():
 
     @login_manager.unauthorized_handler
     def unauthenticated():
-        redirect_url = f"https://github.com/login/oauth/authorize?client_id=(os.getenv('CLIENT_ID'))"
+        redirect_url = f"https://github.com/login/oauth/authorize?client_id={os.getenv('CLIENT_ID')}"
         return redirect(redirect_url)
 
     @login_manager.user_loader
     def load_user(user_id):
-            pass # We will return to this later
+            user = User(user_id)
 
+            return user
     login_manager.init_app(app)    
 
     @app.route('/', methods=['GET', 'POST'])
@@ -32,7 +33,7 @@ def create_app():
         item_view_model = ViewModel(items)
         return render_template('index.html', view_model=item_view_model)      
 
-    @app.route('/callback')
+    @app.route('/login/callback')
     def callback():
         authorisation_code = request.args['code']
         access_token_url = f"https://github.com/login/oauth/access_token"
@@ -56,7 +57,11 @@ def create_app():
 
         user_info_response = requests.get(user_info_url, headers = auth_headers)
 
-    return app
+    class User(UserMixin): 
+        def __init__(self, id): 
+         self.id = id 
+          
+
 
     @app.route('/todo', methods=['GET', 'POST']) 
     @login_required
